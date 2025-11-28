6e97efd7d571009807ccf19d18677e141a43235d
Abdullah <abdullahamer323@gmail.com>
2025-11-28 19:30:42 +0200

    creating http request for products

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit, Type } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "../layout/header/header.component";
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -9,6 +10,35 @@ import { HeaderComponent } from "../layout/header/header.component";
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  baseUrl = "https://localhost:5001/api/"
+  private httpClient = inject(HttpClient);
   title = 'FitNet';
+
+  products: Products[] = [];
+  
+  ngOnInit(): void {
+    this.httpClient.get<Pagination<Products>>(this.baseUrl + "Products").subscribe({
+      next: response => this.products = response.data,
+      error: err => console.log("error" + err),
+      complete: () => console.log("complete")
+    })
+  }
+}
+
+type Products = {
+  id: number,
+  name: string,
+  description: string,
+  price: number,
+  pictureUrl: string,
+  productType: string,
+  productBrand: string
+}
+
+type Pagination<T> = {
+  pageIndex: number,
+  pageSize: number,
+  count: number,
+  data: T[]
 }
