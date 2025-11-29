fd306e4ebe93d57b42fd4fd5683f4f62d35051b3
Abdullah <abdullahamer323@gmail.com>
2025-11-29 20:58:10 +0200

    add product item compnent

---
+++ client/src/features/product-item/product-item.component.ts
@@ -0,0 +1,24 @@
+import { Component, Input, input } from '@angular/core';
+import { Products } from '../../shared/Models/Products';
+import { MatCard, MatCardContent, MatCardActions } from '@angular/material/card'
+import { CurrencyPipe } from '@angular/common';
+import { MatButton } from "@angular/material/button";
+import { MatIcon } from "@angular/material/icon";
+
+@Component({
+  selector: 'app-product-item',
+  standalone: true,
+  imports: [
+    MatCard,
+    MatCardContent,
+    CurrencyPipe,
+    MatCardActions,
+    MatButton,
+    MatIcon
+],
+  templateUrl: './product-item.component.html',
+  styleUrl: './product-item.component.scss'
+})
+export class ProductItemComponent {
+  @Input() product? : Products;
+}
