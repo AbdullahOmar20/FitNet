878f103babadef20ce832b57973b1b496ebc4aca
Abdullah <abdullahamer323@gmail.com>
2025-11-29 20:58:57 +0200

    add shop component and link it to product item component

---
+++ client/src/features/shop/shop.component.ts
@@ -0,0 +1,26 @@
+import { Component, inject, OnInit } from '@angular/core';
+import { ShopService } from '../../core/services/shop.service';
+import { Products } from '../../shared/Models/Products';
+import { ProductItemComponent } from "../product-item/product-item.component";
+
+@Component({
+  selector: 'app-shop',
+  standalone: true,
+  imports: [
+    ProductItemComponent
+],
+  templateUrl: './shop.component.html',
+  styleUrl: './shop.component.scss'
+})
+export class ShopComponent implements OnInit{
+  shopService = inject(ShopService)
+
+  products: Products[] = [];
+  
+  ngOnInit(): void {
+    this.shopService.getProducts().subscribe({
+      next: response => this.products = response.data,
+      error: err => console.log("error" + err)
+    })
+  }
+}
