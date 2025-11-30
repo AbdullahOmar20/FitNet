412644e8fd9ccab4a59c211973ad9b4c4bc733ee
Abdullah <abdullahamer323@gmail.com>
2025-11-30 23:12:32 +0200

    fixing error in console caused by angular dialog material

--- client/src/app/app.config.ts
+++ client/src/app/app.config.ts
@@ -4,12 +4,17 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { provideHttpClient } from '@angular/common/http';
+import { MAT_DIALOG_DATA, MAT_DIALOG_DEFAULT_OPTIONS } from '@angular/material/dialog';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }), 
     provideRouter(routes), 
     provideAnimationsAsync(),
-    provideHttpClient()
+    provideHttpClient(),
+    {
+      provide: MAT_DIALOG_DEFAULT_OPTIONS,
+      useValue: {autoFocus: 'dialog', restoreFocus: true}
+    }
   ]
 };
