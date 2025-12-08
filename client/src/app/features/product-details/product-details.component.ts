491e3171214f26435c42b189e50c71bc3f12b51e
Abdullah <abdullahamer323@gmail.com>
2025-12-08 21:38:44 +0200

    create product details component

---
+++ client/src/app/features/product-details/product-details.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-product-details',
+  standalone: true,
+  imports: [],
+  templateUrl: './product-details.component.html',
+  styleUrl: './product-details.component.scss'
+})
+export class ProductDetailsComponent {
+
+}
