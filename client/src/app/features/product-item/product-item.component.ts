a8057fdd33171c54b9b8690d19409b67eef44107
Abdullah <abdullahamer323@gmail.com>
2025-12-09 21:19:23 +0200

    add routing for product details in product item component

--- client/src/app/features/product-item/product-item.component.ts
+++ client/src/app/features/product-item/product-item.component.ts
@@ -4,6 +4,7 @@ import { MatCard, MatCardContent, MatCardActions } from '@angular/material/card'
 import { CurrencyPipe } from '@angular/common';
 import { MatButton } from "@angular/material/button";
 import { MatIcon } from "@angular/material/icon";
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-product-item',
@@ -14,7 +15,8 @@ import { MatIcon } from "@angular/material/icon";
     CurrencyPipe,
     MatCardActions,
     MatButton,
-    MatIcon
+    MatIcon,
+    RouterLink
 ],
   templateUrl: './product-item.component.html',
   styleUrl: './product-item.component.scss'
