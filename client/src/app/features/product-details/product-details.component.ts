ed8aa1d4aa9b07e8e52bb5fa473d46bdbb900a35
Abdullah <abdullahamer323@gmail.com>
2025-12-09 21:43:01 +0200

    designing product details page

--- client/src/app/features/product-details/product-details.component.ts
+++ client/src/app/features/product-details/product-details.component.ts
@@ -2,11 +2,25 @@ import { Component, inject, OnInit } from '@angular/core';
 import { ShopService } from '../../core/services/shop.service';
 import { ActivatedRoute } from '@angular/router';
 import { Products } from '../../shared/Models/Products';
+import { CurrencyPipe } from '@angular/common';
+import { MatButton } from "@angular/material/button";
+import { MatInput, MatLabel } from "@angular/material/input";
+import { MatIcon } from "@angular/material/icon";
+import { MatFormField } from '@angular/material/form-field';
+import { MatDivider } from "@angular/material/divider";
 
 @Component({
   selector: 'app-product-details',
   standalone: true,
-  imports: [],
+  imports: [
+    CurrencyPipe,
+    MatButton,
+    MatIcon,
+    MatFormField,
+    MatInput,
+    MatLabel,
+    MatDivider
+],
   templateUrl: './product-details.component.html',
   styleUrl: './product-details.component.scss'
 })
