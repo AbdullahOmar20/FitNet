762acf37cb31bb556eda834165179fffdbbc4d0a
Abdullah <abdullahamer323@gmail.com>
2025-12-10 21:46:31 +0200

    add server error to routes

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './features/home/home.component';
 import { ShopComponent } from './features/shop/shop.component';
 import { ProductDetailsComponent } from './features/product-details/product-details.component';
+import { ServerErrorComponent } from './shared/components/server-error/server-error.component';
 
 export const routes: Routes = [
     {path: '', component: HomeComponent},
     {path: 'shop', component: ShopComponent},
     {path: 'shop/:id', component: ProductDetailsComponent},
+    {path: 'server-error', component: ServerErrorComponent},
     {path: '**', redirectTo: "", pathMatch: 'full'},
 ];
