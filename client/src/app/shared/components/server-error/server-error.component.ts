bf8e74909e2e10b2c40f238e249c360978f5ec2a
Abdullah <abdullahamer323@gmail.com>
2025-12-10 21:46:12 +0200

    add server error component

---
+++ client/src/app/shared/components/server-error/server-error.component.ts
@@ -0,0 +1,19 @@
+import { HttpErrorResponse } from '@angular/common/http';
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-server-error',
+  standalone: true,
+  imports: [],
+  templateUrl: './server-error.component.html',
+  styleUrl: './server-error.component.scss'
+})
+export class ServerErrorComponent {
+  error?: HttpErrorResponse
+
+  constructor(private router: Router ){
+    const navigation = this.router.getCurrentNavigation();
+    this.error = navigation?.extras?.state?.['error']
+  }
+}
