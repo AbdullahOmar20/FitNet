332c7a8035e94f8242cb9874bff25fab21182176
Abdullah <abdullahamer323@gmail.com>
2025-12-10 21:26:25 +0200

    add interceptor for errors

--- client/src/app/app.config.ts
+++ client/src/app/app.config.ts
@@ -3,15 +3,16 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
-import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { MAT_DIALOG_DATA, MAT_DIALOG_DEFAULT_OPTIONS } from '@angular/material/dialog';
+import { errorInterceptor } from './core/interceptors/error.interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }), 
     provideRouter(routes), 
     provideAnimationsAsync(),
-    provideHttpClient(),
+    provideHttpClient(withInterceptors([errorInterceptor])),
     {
       provide: MAT_DIALOG_DEFAULT_OPTIONS,
       useValue: {autoFocus: 'dialog', restoreFocus: true}
---
+++ client/src/app/core/interceptors/error.interceptor.ts
@@ -0,0 +1,32 @@
+import { HttpErrorResponse, HttpInterceptorFn } from '@angular/common/http';
+import { inject } from '@angular/core';
+import { Router } from '@angular/router';
+import { catchError, throwError } from 'rxjs';
+import { SnackbarService } from '../services/snackbar.service';
+
+export const errorInterceptor: HttpInterceptorFn = (req, next) => {
+  var router = inject(Router);
+  var snackBar = inject(SnackbarService);
+
+  return next(req).pipe(
+    catchError((err: HttpErrorResponse) => {
+      if(err.status === 400){
+        snackBar.error(err.error.title || err.error)      
+      }
+      if(err.status === 401){
+        snackBar.error(err.error.title || err.error)
+      }
+      if(err.status === 403){
+
+      }
+      if(err.status === 404){
+
+      }
+      if(err.status === 500){
+
+      }
+      
+      return throwError(() => err)
+    }) 
+  );
+};
