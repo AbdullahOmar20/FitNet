0b66c5c986b11e35433ec4a0ea5c8da71f970dbc
Abdullah <abdullahamer323@gmail.com>
2025-12-15 21:35:46 +0200

    add not found page into routes

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -3,11 +3,13 @@ import { HomeComponent } from './features/home/home.component';
 import { ShopComponent } from './features/shop/shop.component';
 import { ProductDetailsComponent } from './features/product-details/product-details.component';
 import { ServerErrorComponent } from './shared/components/server-error/server-error.component';
+import { NotFoundComponent } from './shared/components/not-found/not-found.component';
 
 export const routes: Routes = [
     {path: '', component: HomeComponent},
     {path: 'shop', component: ShopComponent},
     {path: 'shop/:id', component: ProductDetailsComponent},
     {path: 'server-error', component: ServerErrorComponent},
+    {path: 'not-found', component: NotFoundComponent},
     {path: '**', redirectTo: "", pathMatch: 'full'},
 ];
