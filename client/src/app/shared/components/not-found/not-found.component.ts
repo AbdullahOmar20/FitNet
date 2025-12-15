012159d87ffc0c9fdab6d2ab9120d459ed9f270a
Abdullah <abdullahamer323@gmail.com>
2025-12-15 21:35:34 +0200

    add not found page component

---
+++ client/src/app/shared/components/not-found/not-found.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { MatIcon } from "@angular/material/icon";
+import { RouterLink } from "@angular/router";
+import { MatButton } from "@angular/material/button";
+
+@Component({
+  selector: 'app-not-found',
+  standalone: true,
+  imports: [MatIcon, RouterLink, MatButton],
+  templateUrl: './not-found.component.html',
+  styleUrl: './not-found.component.scss'
+})
+export class NotFoundComponent {
+
+}
