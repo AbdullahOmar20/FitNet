c8828c1af43d27eb4da6b402abd36c7e9245554f
Abdullah Omar <abdullahamer323@gmail.com>
2026-01-05 21:42:57 +0200

    add cart component

---
+++ client/src/app/features/cart/cart.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cart',
+  standalone: true,
+  imports: [],
+  templateUrl: './cart.component.html',
+  styleUrl: './cart.component.scss'
+})
+export class CartComponent {
+
+}
