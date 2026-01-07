8e52aac5df99d204e795ddce0b9254ed97727c69
Abdullah Omar <abdullahamer323@gmail.com>
2026-01-07 16:32:17 +0200

    add init service

---
+++ client/src/app/core/services/init.service.ts
@@ -0,0 +1,15 @@
+import { inject, Injectable } from '@angular/core';
+import { CartService } from './cart.service';
+import { of } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class InitService {
+  private cartService = inject(CartService);
+
+  Init(){
+    const cartId = localStorage.getItem('cart_id');
+    const cart$ = cartId ? this.cartService.getCart(cartId) : of(null)
+  }
+}
