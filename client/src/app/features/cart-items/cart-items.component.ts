4f4751b6d41f09fec8a8644e808af43e0b278623
abdullah <abdullahamer323@gmail.com>
2026-01-09 21:48:04 +0200

    add cart items component

---
+++ client/src/app/features/cart-items/cart-items.component.ts
@@ -0,0 +1,16 @@
+import { Component, input } from '@angular/core';
+import { RouterLink } from "@angular/router";
+import { CartItems } from '../../shared/Models/cart';
+import { MatIconButton } from "@angular/material/button";
+import { MatIcon } from "@angular/material/icon";
+import { CurrencyPipe } from '@angular/common';
+
+@Component({
+  selector: 'app-cart-items',
+  imports: [RouterLink, MatIconButton, MatIcon, CurrencyPipe],
+  templateUrl: './cart-items.component.html',
+  styleUrl: './cart-items.component.scss',
+})
+export class CartItemsComponent {
+  item = input.required<CartItems>();
+}
