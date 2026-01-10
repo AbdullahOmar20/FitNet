bd923ef2152a3ce17e010760353a07d2f4cdf8dc
abdullah <abdullahamer323@gmail.com>
2026-01-10 17:09:32 +0200

    add cart summary component to cart

--- client/src/app/features/cart/cart.component.ts
+++ client/src/app/features/cart/cart.component.ts
@@ -1,10 +1,11 @@
 import { Component, inject } from '@angular/core';
 import { CartService } from '../../core/services/cart.service';
 import { CartItemsComponent } from "../cart-items/cart-items.component";
+import { CartSummaryComponent } from "../../shared/components/cart-summary/cart-summary.component";
 
 @Component({
     selector: 'app-cart',
-    imports: [CartItemsComponent],
+    imports: [CartItemsComponent, CartSummaryComponent],
     templateUrl: './cart.component.html',
     styleUrl: './cart.component.scss'
 })
