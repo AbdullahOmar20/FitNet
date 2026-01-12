9eec36719fd276a9f8e696676519f5e628a87d40
abdullah <abdullahamer323@gmail.com>
2026-01-12 18:23:40 +0200

    add update cart functionality to product details

--- client/src/app/features/product-details/product-details.component.ts
+++ client/src/app/features/product-details/product-details.component.ts
@@ -9,6 +9,7 @@ import { MatIcon } from "@angular/material/icon";
 import { MatFormField } from '@angular/material/form-field';
 import { MatDivider } from "@angular/material/divider";
 import { CartService } from '../../core/services/cart.service';
+import { FormsModule } from '@angular/forms';
 
 @Component({
     selector: 'app-product-details',
@@ -19,7 +20,8 @@ import { CartService } from '../../core/services/cart.service';
         MatFormField,
         MatInput,
         MatLabel,
-        MatDivider
+        MatDivider,
+        FormsModule
     ],
     templateUrl: './product-details.component.html',
     styleUrl: './product-details.component.scss'
@@ -32,6 +34,8 @@ export class ProductDetailsComponent implements OnInit {
   private activatedRtoue = inject(ActivatedRoute);
   
   product?: Products
+  Quantity = 1;
+  QuantityInCart = 0;
 
   ngOnInit(): void {
     this.loadProduct();
@@ -42,8 +46,40 @@ export class ProductDetailsComponent implements OnInit {
     if(!id) return;
 
     this.shopService.getProduct(+id).subscribe({
-      next: product => this.product = product,
+      next: product => {
+        this.product = product;
+        this.updateQuantityInCart();
+      },
       error: err => console.log(err)
     })
+  }
+
+  updateQuantityInCart(){
+    if(!this.product) return;
+    this.QuantityInCart = this.cartService.cart()?.items.find(x => x.id === this.product?.id)?.quantity || 0;
+    
+    this.Quantity = this.QuantityInCart || 1;
+
+    // this.cartService.addItemToCart(this.product, this.Quantity);
+    // this.QuantityInCart = this.Quantity;
+  }
+
+  getButtonText(){
+    return this.QuantityInCart > 0 ? 'Update cart' : 'Add to cart'
+  }
+
+  updateCart(){
+    if(!this.product) return;
+    if(this.Quantity > this.QuantityInCart){
+      const itemsToBeAdd = this.Quantity - this.QuantityInCart
+      this.QuantityInCart += itemsToBeAdd;
+      this.cartService.addItemToCart(this.product, itemsToBeAdd);
+    }
+    else{
+      const itemsToBeRemoved = this.QuantityInCart - this.Quantity;
+      this.QuantityInCart -= itemsToBeRemoved;
+      this.cartService.removeItemFromCart(this.product.id, itemsToBeRemoved);
+    }
+    
   }
 }
